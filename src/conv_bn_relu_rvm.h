d75e7531f372e4c640cecd2fa0f6cb104fd201e9
kening.zhang <kening.zhang@streamcomputing.com>
2023-01-10 18:43:23 +0800

    add conv_bn_relu ops

---
+++ src/conv_bn_relu_rvm.h
@@ -0,0 +1,135 @@
+#ifndef __CONV_BN_RELU_H__
+#define __CONV_BN_RELU_H__
+
+#include "tensor.h"
+#include <stddef.h>
+
+#include "mme.h"
+#include "matmul.h"
+
+static inline int conv_bn_relu(Tensor *dst, Tensor *src, Tensor *weight, Tensor *alpha, Tensor *beta, Config *ss)
+{
+    int stride_h = ss->stride_h;
+    int stride_w = ss->stride_w;
+
+    int pad_t = ss->top;
+    int pad_b = ss->bottom;
+    int pad_l = ss->left;
+    int pad_r = ss->right;
+
+    int dilation_h = ss->dilation_h;
+    int dilation_w = ss->dilation_w;
+
+    int kh = ss->kh;
+    int kw = ss->kw;
+
+    int hin = ss->hin;
+    int win = ss->win;
+    int cin = ss->cin;
+
+    int hout = ss->hout;
+    int wout = ss->wout;
+    int cout = ss->cout;
+
+    int dataSize = sizeof(float16_t);
+    float16_t *psrc1 = (float16_t *)src->data;
+    float16_t *psrc2 = (float16_t *)weight->data;
+    float16_t *pdst = (float16_t *)dst->data;
+    float16_t *palpha = (float16_t *)alpha->data;
+    float16_t *pbeta = (float16_t *)beta->data;
+
+    int mtype = 1;
+    asm volatile("msettype x0, %[rs1]"
+                : 
+                : [rs1]"r"(mtype));
+
+    int moutsh = hout << 16 | wout;
+    int minsh = hin << 16 | win;
+    int mpad = pad_t << 24 | pad_b << 16 | pad_l << 8 | pad_r;
+    int mstdi = dilation_h << 24 | dilation_w << 16 | stride_h << 8 | stride_w;
+
+    int m = hout * wout;
+    int k = kh *kw * cin;
+    int n = cout;
+
+    int tilem, tilen, tilek;
+
+    asm volatile("msetoutsh x0, %[rs1], %[rs2]"
+                : 
+                : [rs1]"r"(moutsh), [rs2]"r"(mstdi));
+    asm volatile("msetinsh x0, %[rs1], %[rs2]"
+                :
+                : [rs1]"r"(minsh), [rs2]"r"(mpad));
+    // conv
+    for (int i = 0; i < m; i+=tilem) {
+      asm volatile("msettilem %[rd], %[rs1]"
+                    : [rd]"=r"(tilem)
+                    : [rs1]"r"(m-i));
+
+      int hout_pos = i / wout;
+      int wout_pos = i - hout_pos * wout;
+      
+      for (int j = 0; j < n; j+=tilen) {
+        asm volatile("msettilen %[rd], %[rs1]"
+                        : [rd]"=r"(tilen)
+                        : [rs1]"r"(n-j));
+        asm volatile("mwsubc.mm acc0, acc0");
+
+        for (int skh = 0; skh < kh; skh++) {
+          int hin_pos = hout_pos * stride_h - pad_t + skh * dilation_h;
+          for (int skw = 0; skw < kw; skw++) {
+            int win_pos = wout_pos * stride_w - pad_l + skw * dilation_w;
+            asm volatile("msetsk x0, %[rs1], %[rs2]"
+                        : 
+                        : [rs1]"r"(hin_pos <<  16 | (win_pos & 0xffff)), [rs2]"r"((skw * dilation_w) << 16 | wout_pos));
+            float16_t *_prsc1 = psrc1+hin_pos*win*cin+win_pos*cin;
+            float16_t *_psrc2 = psrc2+skh*kw*cin*cout+skw*cin*cout+j;
+            for (int skc = 0; skc < cin;  skc+=tilek) {
+                asm volatile("msettilek %[rd], %[rs1]"
+                            : [rd]"=r"(tilek)
+                            : [rs1]"r"(cin-skc));
+                asm volatile("mlufae16.m tr0, (%[rs1]), %[rs2]"
+                            :
+                            :[rs1]"r"(_prsc1+skc), [rs2]"r"(cin*dataSize));
+                
+                asm volatile("mlbe16.m tr1, (%[rs1]), %[rs2]"
+                            :
+                            :[rs1]"r"(_psrc2+skc*cout), [rs2]"r"(cout*dataSize));
+                asm volatile("mfwma.mm acc0, tr0, tr1");
+            }
+          }
+        }
+
+        // batchnormal
+        int vl = vsetvl_e16m1(tilen);
+        asm volatile("vle16.v v8, (%[rs1])"
+                    : 
+                    : [rs1]"r"(palpha + j));
+        asm volatile("vle16.v v16, (%[rs1])"
+                    : 
+                    : [rs1]"r"(pbeta + j));
+        asm volatile("mfwmacccr.mv acc0, v8, v16");
+        asm volatile("mfncvtc.f.fw.m acc1, acc0");
+        
+        // relu
+        for (int k = 0; k < tilem; k+=8) {
+          int lmul = min(8, tilem-k);
+          vl = vsetvl_e16m8(tilen*lmul);
+          asm volatile("mmvcr.v.m v24, acc1, %[rs2]"
+                        :
+                        : [rs2]"r"(k));
+          asm volatile("vfmax.vf v0, v24, %[frs2]"
+                        :
+                        : [frs2]"f"((float16_t)0.f));
+          asm volatile("msce16.v v0, (%[rs1]), %[rs2]"
+                        :
+                        : [rs1]"r"(pdst+(i+k)*cout+j), [rs2]"r"(cout*dataSize));
+        }
+      }
+    }
+
+    
+    return 0;
+}
+
+#endif //__CONV_BN_RELU_H__
---
+++ test/ops/conv_add_bn_relu/test.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../../src/conv_add_bn_relu_rvm.h"
+#include "../../../src/perf.h"
+#include "../../../include/incbin.h"
+
+#include "params.h"
+
+INCBIN(srcData, "src.bin", ".scdata.params");
+INCBIN(weightData, "weight.bin", ".scdata.params");
+INCBIN(addsrcData, "addsrc.bin", ".scdata.params");
+INCBIN(alphaData, "alpha.bin", ".scdata.params");
+INCBIN(betaData, "beta.bin", ".scdata.params");
+
+uint8_t dstData[OUT_SIZE * sizeof(float16_t)] __attribute__((__section__(".scdata.output")));
+
+
+int main(int argc, char **argv)
+{
+    printf("Begin\n");
+
+    config_conv(sst, HIN, WIN, CIN, COUT, PAD_TOP, PAD_BOTTOM, PAD_LEFT, PAD_RIGHT, KH, KW, STRIDE_H, STRIDE_W, DILATION_H, DILATION_W);
+
+    tensor_new_2d(srcMat, HIN, WIN * CIN, sizeof(float16_t), srcData);
+    tensor_new_2d(weightMat, KH * KW * CIN, COUT, sizeof(float16_t), weightData);
+    tensor_new_2d(addsrcMat, HOUT * WOUT, COUT, sizeof(float32_t), addsrcData);
+    tensor_new_1d(alphaMat, COUT, sizeof(float16_t), &alphaData);
+    tensor_new_1d(betaMat, COUT, sizeof(float16_t), &betaData);
+    tensor_new_2d(dstMat, HOUT * WOUT, COUT, sizeof(float16_t), &dstData);
+
+    PERF_BEGIN();
+
+    for (int i = 0; i < NLOOPS; i++) {
+        conv_add_bn_relu(&dstMat, &srcMat, &weightMat, &addsrcMat, &alphaMat, &betaMat, &sst);
+    }
+
+    PERF_END();
+    asm("fence.i");
+    if (DEBUG_PRINT) {
+        printf("Out shape: \n\t(hout, wout, cout) = (%d, %d, %d)\n",
+                sst.hout, sst.wout, sst.cout);
+    }
+
+    printf("End\n");
+
+    return 0;
+}
